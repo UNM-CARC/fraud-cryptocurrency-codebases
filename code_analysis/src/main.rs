12de56e39428f0ad45d40468e67b386c87bb91c3
Keira Haskins <wasp@unm.edu>
2019-09-09 18:01:01 -0600

    Working on building a directory hash tree in Rust.

--- code_analysis/src/main.rs
+++ code_analysis/src/main.rs
@@ -1,3 +1,38 @@
+extern crate csv;
+#[macro_use]
+extern crate serde_derive;
+
+//use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use csv::Error;
+use std::process;
+
+//#[derive(Deserialize)]
+//struct Record {
+//    id: String,
+//    name: String,
+//    repo: String,
+//}
+
+type Record = (String, String, String);
+
+fn run() -> Result<(), Error> {
+    let file = File::open("names.csv")?;
+    //let mut buff = [0;1024];
+    //let csv      = f.read(&mut buff)?;
+
+    let mut reader = csv::Reader::from_reader(file);
+    for result in reader.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Code Analysis");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
