c0aeeb631ddb07832e796746564c7d75dd617418
Keira Haskins <wasp@unm.edu>
2019-09-12 11:22:50 -0600

    Have been struggling with exceptions in Haskell. Have reached about the same point with Rust now.

--- code_analysis/src/main.rs
+++ code_analysis/src/main.rs
@@ -1,12 +1,19 @@
 extern crate csv;
 #[macro_use]
 extern crate serde_derive;
+extern crate walkdir;
+extern crate sha2;
+extern crate data_encoding;
+extern crate ring;
 
 //use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use csv::Error;
 use std::process;
+use walkdir::{DirEntry, WalkDir};
+
+mod util;
 
 //#[derive(Deserialize)]
 //struct Record {
@@ -14,25 +21,22 @@ use std::process;
 //    name: String,
 //    repo: String,
 //}
-
-type Record = (String, String, String);
-
-fn runcsv() -> Result<(), Error> {
-    let file = File::open("names.csv")?;
-    //let mut buff = [0;1024];
-    //let csv      = f.read(&mut buff)?;
-
-    let mut reader = csv::Reader::from_reader(file);
-    for result in reader.deserialize() {
-        let record: Record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+         .file_name()
+         .to_str()
+         .map(|s| entry.depth() == 0 || !s.starts_with("."))
+         .unwrap_or(false)
 }
 
 fn main() {
-    if let Err(err) = runcsv() {
-        println!("{}", err);
-        process::exit(1);
-    }
+    //if let Err(err) = util::runcsv() {
+    //    println!("{}", err);
+    //    process::exit(1);
+    //}
+    WalkDir::new("/tmp/BTC")
+        .into_iter()
+        .filter_entry(|e| is_not_hidden(e))
+        .filter_map(|v| v.ok())
+        .for_each(|x| println!("{}", x.path().display()));
 }
