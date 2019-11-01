1f7b07bbaf83f24c33a8863f15761afc7022a8d9
Keira Haskins <keirahaskins26@gmail.com>
2019-11-01 10:21:17 -0600

    Serialized asts into strings ParseTrees.hs.

--- src/ParseTrees.hs
+++ src/ParseTrees.hs
@@ -7,6 +7,8 @@ import qualified Data.GraphViz.Types as T
 import qualified Data.Text    as Txt
 import qualified Data.Text.IO as Txt
 --import qualified Data.Text.Internal.Lazy as Txt
+import           Data.List
+import           Data.Ord (comparing)
 import           Data.GraphViz.Types.Generalised
 import           System.Process
 import           System.FilePath.Posix
@@ -18,18 +20,63 @@ import           Language.C.Clang.Cursor
 --import DotParser
 --import DotPretty
 
+--subStrings :: [a] -> [[a]]
+--subStrings s =
+--  let intChars = length s
+--  in [ take n $ drop i s
+--     | i <- [0 .. intChars - 1] 
+--     , n <- [1 .. intChars - i] ]
+-- 
+--longestCommon :: Eq a => [a] -> [a] -> [a]
+--longestCommon a b =
+--  maximumBy (comparing length) (subStrings a `intersect` subStrings b)
 
+longestCommon :: Eq a => [a] -> [a] -> [a]
+longestCommon a b =
+  maximumBy (comparing length) $
+  (uncurry intersect . pair) $ [tail . inits <=< tails] <*> [a, b]
+ 
+pair :: [a] -> (a, a)
+pair [x, y] = (x, y)
+
+--parseCPP :: FilePath -> [Cursor]
 parseCPP file = do
     idx <- createIndex
-    tu <- parseTranslationUnit idx file ["-I/usr/local/include"]
+    tu  <- parseTranslationUnit idx file ["-I/usr/local/include"]
     let root = translationUnitCursor tu
         --children = cursorChildren root
         children = cursorDescendants root
         --functionDecls = filter (\c -> cursorKind c == FunctionDecl) children
     --forM_ children (print . cursorSpelling)
     --print root
-    print $ map cursorKind children
+    --print $ map cursorKind children
+    mapM_ print $ map cursorKind children
 
+treeToString file = do
+  idx <- createIndex
+  tu  <- parseTranslationUnit idx file ["-I/usr/local/include"]
+  let root = translationUnitCursor tu
+      str  = serialize root
+  print str
+
+serialize :: Cursor -> String
+serialize root = '(' : (show $ cursorKind root) 
+                    ++ (foldl (\acc y -> acc ++ serialize y) "" (cursorChildren root))
+                    ++ ")"
+
+{-
+serializeCPP file = do
+    idx <- createIndex
+    tu  <- parseTranslationUnit idx file ["-I/usr/local/include"]
+    let root = translationUnitCursor tu
+        children = cursorChildren root
+        
+        --functionDecls = filter (\c -> cursorKind c == FunctionDecl) children
+    --forM_ children (print . cursorSpelling)
+    --print root
+    --print $ map cursorKind children
+    mapM_ print $ map cursorKind children
+-}
 
 generateAST repo file = do
   DIR.createDirectoryIfMissing True ("/tmp/AST/" ++ repo ++ "/")
