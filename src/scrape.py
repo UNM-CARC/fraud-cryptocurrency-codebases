7ac4dbef2d3b207108aec783d57e8f2315e81c44
Keira Haskins <keirahaskins26@gmail.com>
2021-05-03 11:41:25 -0600

    added functionality to Lib.hs.

--- src/scrape.py
+++ src/scrape.py
@@ -9,10 +9,18 @@ import json
 # url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info'
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
+data_final = "/home/ghostbird/Hacking/cybersecurity/fraud-cryptocurrency-codebases/data_final/data_final.csv"
+# data_final = "/carc/scratch/projects/bridges2016099/data_final/data_final.csv"
+df_data_final = pd.read_csv(data_final)
+# df_data_final.set_index("name", inplace=True)
+# print(df_data_final)
 
 cmc = CoinMarketCapAPI("f43cf05e-ba8f-42f8-8c8c-d8dbe6c7531b")
 data_id_map = cmc.cryptocurrency_listings_latest()
+# data_id_map = cmc.cryptocurrency_info(symbol="BTC")
 normalized = pd.json_normalize(data_id_map.data)
 df = pd.DataFrame(normalized)
-df.set_index("name", inplace=True)
-print(df.head())
+df.set_index("slug", inplace=True)
+# print(df)
+df_merge = pd.merge(df_data_final, df, on="slug")
+print(df_merge)
